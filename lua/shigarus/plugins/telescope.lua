d8095285c9e8d2da6098489e6a9374cebc7bea26
shigarus <shiggarus@gmail.com>
2024-11-04 20:40:53 +0100

    show good hidden files

--- lua/shigarus/plugins/telescope.lua
+++ lua/shigarus/plugins/telescope.lua
@@ -123,6 +123,7 @@ return {
 
     -- Enable Telescope extensions if they are installed
     pcall(telescope.load_extension, 'fzf')
+    pcall(telescope.load_extension, 'fd')
     pcall(telescope.load_extension, 'ui-select')
     pcall(telescope.load_extension, 'live_grep_args')
     pcall(telescope.load_extension, 'undo')
@@ -131,7 +132,9 @@ return {
     local builtin = require 'telescope.builtin'
     vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sf', function()
+      builtin.find_files { hidden = true, find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' } }
+    end, { desc = '[S]earch [F]iles' })
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', function()
