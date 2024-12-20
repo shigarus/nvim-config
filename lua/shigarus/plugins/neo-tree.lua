5b9edc04ec16dd0db66ebb8529a231c50fdab6bb
shigarus <shiggarus@gmail.com>
2024-12-20 15:23:36 +0100

    switch to oil by default

--- lua/shigarus/plugins/neo-tree.lua
+++ lua/shigarus/plugins/neo-tree.lua
@@ -13,15 +13,4 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
-  lazy = false,
-  opts = {
-    filesystem = {
-      hijack_netrw_behavior = 'open_current',
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
 }
