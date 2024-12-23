31e281c082a74d5681e034882b275fa2cc04dc16
shigarus <shiggarus@gmail.com>
2024-12-23 12:04:52 +0100

    redefine keymaps for oil to not conflict with switching bween windows shortcuts

--- lua/shigarus/plugins/oil.lua
+++ lua/shigarus/plugins/oil.lua
@@ -2,6 +2,22 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    use_default_keymaps = false,
+    keymaps = {
+      ['g?'] = { 'actions.show_help', mode = 'n' },
+      ['<CR>'] = 'actions.select',
+      ['<C-p>'] = 'actions.preview',
+      ['<C-c>'] = { 'actions.close', mode = 'n' },
+      ['-'] = { 'actions.parent', mode = 'n' },
+      ['_'] = { 'actions.open_cwd', mode = 'n' },
+      ['`'] = { 'actions.cd', mode = 'n' },
+      ['~'] = { 'actions.cd', opts = { scope = 'tab' }, mode = 'n' },
+      ['gs'] = { 'actions.change_sort', mode = 'n' },
+      ['gx'] = 'actions.open_external',
+      ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
+      ['g\\'] = { 'actions.toggle_trash', mode = 'n' },
+    },
+  },
   dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if prefer nvim-web-devicons
 }
