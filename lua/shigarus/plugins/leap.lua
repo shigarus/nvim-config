16a669a7749bba201dc80bc122d11d37ac6b09b3
shigarus <shiggarus@gmail.com>
2025-01-27 19:24:29 +0100

    leap anywhere

--- lua/shigarus/plugins/leap.lua
+++ lua/shigarus/plugins/leap.lua
@@ -2,9 +2,10 @@ return {
   'ggandor/leap.nvim',
   config = function()
     local leap = require 'leap'
-    vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
-    vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)')
-    vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)')
+    vim.keymap.set('n', 's', '<Plug>(leap-anywhere)')
+    vim.keymap.set('x', 's', '<Plug>(leap)')
+    vim.keymap.set('o', 's', '<Plug>(leap-forward)')
+    vim.keymap.set('o', 'S', '<Plug>(leap-backward)')
     leap.opts.case_sensetive = true
   end,
 }
