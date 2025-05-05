14cb04c8eb997be9de9baf041a63b32e1408d128
shigarus <shiggarus@gmail.com>
2025-05-05 22:12:40 +0200

    make status line nicer

--- lua/shigarus/plugins/lualine.lua
+++ lua/shigarus/plugins/lualine.lua
@@ -3,12 +3,46 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = { theme = 'monokai' },
-  -- config = function()
-  --   local monokai = require 'nvim-lualine/lualine.themes.monokai'
-  --   monokai.normal.c.bg = '#972822'
-  --   require('lualine').setup {
-  --     options = { theme = monokai },
-  --   }
-  -- end,
+  config = function()
+    local lualine = require 'lualine'
+    local colors = {
+      black = '#2C2A2E',
+      gray = '#403E41',
+      white = '#F6F6F5',
+      green = '#A6CA7B',
+
+      orange = '#EE9D70',
+      pink = '#A69BE8',
+      red = '#EB6B88',
+      yellow = '#F9D978',
+    }
+
+    local theme = {
+      normal = {
+        a = { fg = colors.black, bg = colors.yellow, gui = 'bold' },
+        b = { fg = colors.yellow, bg = colors.gray },
+        c = { fg = colors.white, bg = colors.black },
+      },
+      insert = { a = { fg = colors.black, bg = colors.green, gui = 'bold' } },
+      visual = { a = { fg = colors.black, bg = colors.pink, gui = 'bold' } },
+      replace = { a = { fg = colors.black, bg = colors.red, gui = 'bold' } },
+      inactive = {
+        a = { fg = colors.black, bg = colors.yellow, gui = 'bold' },
+        b = { fg = colors.black, bg = colors.yellow },
+        c = { fg = colors.white, bg = colors.black },
+      },
+    }
+    lualine.setup {
+      options = { theme = theme },
+      extensions = { 'fzf', 'oil', 'quickfix' },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            path = 1,
+          },
+        },
+      },
+    }
+  end,
 }
