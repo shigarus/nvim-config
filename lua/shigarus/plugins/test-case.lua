cd79c2d00dfc8610135f644f2a8808d327a42325
shigarus <shiggarus@gmail.com>
2025-05-21 16:14:07 +0200

    remove clutter from text-case

--- lua/shigarus/plugins/test-case.lua
+++ lua/shigarus/plugins/test-case.lua
@@ -3,15 +3,4 @@ return {
   config = function()
     require('textcase').setup {}
   end,
-  keys = {
-    'ga', -- Default invocation prefix
-    { 'gau', "<cmd>lua require('textcase').current_word('to_upper_case')<CR>", mode = { 'n', 'x' } },
-    { 'gaU', "<cmd>lua require('textcase').lsp_rename('to_upper_case')<CR>", mode = { 'n', 'x' } },
-    { 'gal', "<cmd>lua require('textcase').current_word('to_lower_case')<CR>", mode = { 'n', 'x' } },
-    { 'gaL', "<cmd>lua require('textcase').lsp_rename('to_lower_case')<CR>", mode = { 'n', 'x' } },
-    { 'gas', "<cmd>lua require('textcase').current_word('to_snake_case')<CR>", mode = { 'n', 'x' } },
-    { 'gaS', "<cmd>lua require('textcase').lsp_rename('to_snake_case')<CR>", mode = { 'n', 'x' } },
-    { 'gap', "<cmd>lua require('textcase').current_word('to_pascal_case')<CR>", mode = { 'n', 'x' } },
-    { 'gaP', "<cmd>lua require('textcase').lsp_rename('to_pascal_case')<CR>", mode = { 'n', 'x' } },
-  },
 }
