3b7429a786d54568099b4e68bb71e9989488d459
Aleksandr Stovbyra <shigarus@nebius.com>
2025-05-30 18:37:49 +0200

    annoying

--- lua/shigarus/plugins/lsps.lua
+++ lua/shigarus/plugins/lsps.lua
@@ -56,7 +56,7 @@ return {
       callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
         if client:supports_method 'textDocument/completion' then
-          vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+          vim.lsp.completion.enable(true, client.id, ev.buf)
         end
       end,
     })
