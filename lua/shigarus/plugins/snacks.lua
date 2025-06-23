a70cbb629e4d34355cbd5db5d141d40c449f8872
Aleksandr Stovbyra <shigarus@nebius.com>
2025-06-23 17:53:26 +0200

    add gitbrowse

--- lua/shigarus/plugins/snacks.lua
+++ lua/shigarus/plugins/snacks.lua
@@ -5,6 +5,8 @@ return {
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
+    git = { enabled = true },
+    gitbrowse = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
     -- conflicts with noice
@@ -13,5 +15,22 @@ return {
     -- scope = { enabled = true },
     -- scroll = { enabled = true },
     words = { enabled = true },
+  },
+  keys = {
+    {
+      '<leader>gB',
+      function()
+        Snacks.gitbrowse()
+      end,
+      desc = 'Git Browse',
+      mode = { 'n', 'v' },
+    },
+    {
+      '<leader>gg',
+      function()
+        Snacks.lazygit()
+      end,
+      desc = 'Lazygit',
+    },
   },
 }
