1d59e4336b1a9d7af8db58c49c0d0b08eca5b0b6
shigarus <shiggarus@gmail.com>
2025-07-11 14:50:51 +0200

    better notes

--- init.lua
+++ init.lua
@@ -203,4 +203,10 @@ if vim.env.IS_NOTES == 'true' then
   ]]
   vim.opt.signcolumn = 'no'
   vim.o.autowriteall = true
+  -- Enable autoread and set up checking triggers
+  vim.o.autoread = true
+  vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter' }, {
+    command = "if mode() != 'c' | checktime | endif",
+    pattern = '*',
+  })
 end
